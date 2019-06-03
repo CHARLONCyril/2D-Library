fe2221281eed9681e23c2f5a32d1e70646b6cd28
erictanggit <eric.tang@dauphine.eu>
2019-06-03 22:41:33 +0200

    Conflict correction

--- src/test/java/io/github/oliviercailloux/twod_library/controller/ConnectionToCongressLibraryTest.java
+++ src/test/java/io/github/oliviercailloux/twod_library/controller/ConnectionToCongressLibraryTest.java
@@ -1,9 +1,6 @@
 package io.github.oliviercailloux.twod_library.controller;
 
 import static org.junit.Assert.*;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
 import io.github.oliviercailloux.twod_library.controller.ConnectionToCongressLibrary;
 
@@ -17,13 +14,12 @@ public class ConnectionToCongressLibraryTest {
 	 * The REST connection under test
 	 */
 	ConnectionToCongressLibrary connection;
-
-	public void setUp() {
-
+	
+	public void setUp(){
 		String code = "70108638";
 		connection = new ConnectionToCongressLibrary(code);
 	}
-
+	
 	public void getMarcXML_Should_Return_The_Marcxml_In_A_String() {
 		String actual = connection.getMarcXML();
 		CharSequence s1 = "French profiles.";
@@ -36,16 +32,16 @@ public class ConnectionToCongressLibraryTest {
 		boolean b3 = actual.contains(s3);
 		assertTrue(b3);
 	}
-
-	public void extractData_Should_Returntitle_Author_and_Year_In_A_Tab_Of_String() {
-
+	
+	public void extractData_Should_Returntitle_Author_and_Year_In_A_Tab_Of_String(){
 		String[] actual = connection.extractData();
 		assertEquals("French profiles.", actual[0]);
 		assertEquals("Gosse, Edmund,", actual[1]);
 		assertEquals("[1970", actual[2]);
 	}
 
-	public void afterTest() {
+
+	public void afterTest(){
 		connection = null;
 	}
 
