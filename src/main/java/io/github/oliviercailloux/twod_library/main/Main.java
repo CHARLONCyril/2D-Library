a373c2971461c511a12f09093e08b796a3ba6a22
CHARLONCyril <cyril0308@gmail.com>
2019-06-29 01:43:39 +0200

    update GUI

--- src/main/java/io/github/oliviercailloux/twod_library/main/Main.java
+++ src/main/java/io/github/oliviercailloux/twod_library/main/Main.java
@@ -1,12 +1,14 @@
 package io.github.oliviercailloux.twod_library.main;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.github.oliviercailloux.twod_library.controller.CSVUtils;
 import io.github.oliviercailloux.twod_library.controller.DataFile;
 import io.github.oliviercailloux.twod_library.model.Library;
 import io.github.oliviercailloux.twod_library.view.SVGLibrary;
@@ -16,7 +18,8 @@ public class Main {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-	public static void main(String[] args) throws IOException, ParserConfigurationException {
+	public static void main(String[] args) throws IOException, ParserConfigurationException, URISyntaxException {
+		CSVUtils.initializeSettingFile("DefaultUserPreference.csv");
 		DataFile dataFile = new DataFile();
 		Library library = new Library(dataFile.read(), 18);
 		SVGLibrary svgLibrary = new SVGLibrary(library);
