a7075ddf9b4d2b6fda9d49ef0127595da410be15
HenryJulienDesgranges <henry-julien.desgranges@orange.fr>
2019-06-29 21:33:48 +0200

    remove PublicationRange and replace it by Range<Integer>

---
+++ src/main/java/io/github/oliviercailloux/twod_library/model/SearchData.java
@@ -0,0 +1,43 @@
+package io.github.oliviercailloux.twod_library.model;
+
+import org.glassfish.jersey.internal.guava.MoreObjects;
+
+public final class SearchData {
+
+	private final String authorRegex;
+	private final PublicationRange publication;
+	private final String titleRegex;
+
+	public String getAuthorRegex() {
+		return authorRegex;
+	}
+
+	public PublicationRange getPublication() {
+		return publication;
+	}
+
+	public String getTitleRegex() {
+		return titleRegex;
+	}
+
+	private SearchData(String authorRegex1, PublicationRange publication, String titleRegex) {
+		this.authorRegex = authorRegex1;
+		this.publication = publication;
+		this.titleRegex = titleRegex;
+	}
+
+	public static SearchData createSearchDataObject(String authorRegex, PublicationRange publication,
+			String titleRegex) {
+		return new SearchData(authorRegex, publication, titleRegex);
+	}
+
+	/**
+	 * using to represent the object SearchData
+	 */
+	public String toString() {
+
+		return MoreObjects.toStringHelper(this).add("authorRegex", getAuthorRegex())
+				.add("publication", getPublication()).add("titleRegex", getTitleRegex()).toString();
+	}
+
+}
